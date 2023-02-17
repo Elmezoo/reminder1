9afe3bf46249698423a35cc1ec0612fcc9380b2e
mohamed <mi2355148@gmail.com>
2023-02-17 14:42:05 +0200

    hello mizo

---
+++ lib/shared/components/constants.dart
@@ -0,0 +1,5 @@
+
+
+//base url : https://www.google.com/
+// method url : search?
+// Qoeries :  q=news+api&oq=news+api&aqs=chrome.0.69i59j0i512l3j0i22i30i625l2j69i60l2.10487j0j7&sourceid=chrome&ie=UTF-8
